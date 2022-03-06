1e30e3abd894f6b9311a1e9e5ced9fcea3762f0f
Peyman Tahghighi <tahghighipeyman@gmail.com>
2022-03-06 10:13:30 -0500

    No change, just changing directory

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 #================================================================
 #================================================================
+from copyreg import pickle
 import os
 from glob import glob
-from django import conf
 from sklearn.model_selection import KFold
 import numpy as np
-
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import torch.optim as optim
 import torch.nn as nn
 import torch
+import pickle
 from torchmetrics import Precision, Recall, F1, StatScores
 import Config
 from Dataset import MRIData, dataset_slicer, load_entire_MRI
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     torch.autograd.profiler.emit_nvtx(False)
     torch.backends.cudnn.benchmark = True
     
-    #dataset_slicer(root_path="D:\PhD\Courses\Image Analysis\Project\Dataset\Pure", output_root="D:\PhD\Courses\Image Analysis\Project\Dataset\\2D slice");
+    dataset_slicer(root_path="..\\feta", output_root="...\\2D slice");
     entire_dataset = np.array(glob("D:\PhD\Courses\Image Analysis\Project\Dataset\\2D slice\\*"));
 
     #for k-fold cross-validation
@@ -38,97 +38,108 @@ if __name__ == "__main__":
     loss_func = nn.CrossEntropyLoss();
     scaler = torch.cuda.amp.grad_scaler.GradScaler()
 
+    # total_data = [];
+    # for train,test in kfold.split(entire_dataset):
+    #     total_data.append([train,test]);
+    
+    # pickle.dump(total_data, open("fold_data.fd", 'wb'));
+
     #define estimator of the model
     stat_scores = StatScores(num_classes=8, reduce='macro').to(Config.DEVICE);
 
     #for holding fold number
     f = 0;
-    for train,test in kfold.split(entire_dataset):
-        print(f"\n==========================================================================\n\
-            Starting fold {f}...\
-                \n==========================================================================");
 
-        #Get data and generate data loaders for this batch
-        train_folders = np.take(entire_dataset,train);
-        test_folders = np.take(entire_dataset,test);
+    #load fold from file
+    folds = pickle.load(open("fold_data.fd", "rb"));
+    train = folds[0][0];
+    test = folds[0][1];
+    #for train,test in kfold.split(entire_dataset):
+    print(f"\n==========================================================================\n\
+        Starting fold {f}...\
+            \n==========================================================================");
 
-        train_data = load_entire_MRI(train_folders);
-        test_data = load_entire_MRI(test_folders);
+    #Get data and generate data loaders for this batch
+    train_folders = np.take(entire_dataset,train);
+    test_folders = np.take(entire_dataset,test);
 
-        train_loader = DataLoader(MRIData(train_data,
-                                    Config.train_transforms),
-                                    batch_size=Config.BATCH_SIZE,
-                                    num_workers = Config.NUM_WORKERS,
-                                    pin_memory=True);
+    train_data = load_entire_MRI(train_folders);
+    test_data = load_entire_MRI(test_folders);
 
-        test_loader = DataLoader(MRIData(test_data,
-                                    Config.valid_transforms),
-                                    batch_size=Config.BATCH_SIZE,
-                                    num_workers = Config.NUM_WORKERS,
-                                    pin_memory=True);
-        #---------------------------------------------------------------
+    train_loader = DataLoader(MRIData(train_data,
+                                Config.train_transforms),
+                                batch_size=Config.BATCH_SIZE,
+                                num_workers = Config.NUM_WORKERS,
+                                pin_memory=True);
 
-        #reset model parameters each time we want to run a new fold
-        model.reset_weights();
+    test_loader = DataLoader(MRIData(test_data,
+                                Config.valid_transforms),
+                                batch_size=Config.BATCH_SIZE,
+                                num_workers = Config.NUM_WORKERS,
+                                pin_memory=True);
+    #---------------------------------------------------------------
 
-        #epoch counter
-        e = 1;
-        #for early stopping
-        best_loss = 10;
-        best_prec = 0;
-        best_rec = 0;
-        best_f1 = 0;
-        best_vs = 0;
-        patience = Config.EARLY_STOPPING_PATIENCE;
-        while(True):
+    #reset model parameters each time we want to run a new fold
+    model.reset_weights();
 
-            train_one_epoch(model, train_loader, loss_func, optimizer, scaler);
+    #epoch counter
+    e = 1;
+    #for early stopping
+    best_loss = 10;
+    best_prec = 0;
+    best_rec = 0;
+    best_f1 = 0;
+    best_vs = 0;
+    patience = Config.EARLY_STOPPING_PATIENCE;
+    while(True):
 
-            total_train_loss, prec_train, rec_train, f1_train, vs_train = eval_one_epoch(model, 
-            train_loader, 
-            loss_func,
-            stat_scores);
+        train_one_epoch(model, train_loader, loss_func, optimizer, scaler);
 
-            total_test_loss, prec_test, rec_test, f1_test, vs_test = eval_one_epoch(model, 
-            test_loader, 
-            loss_func,
-            stat_scores);
+        total_train_loss, prec_train, rec_train, f1_train, vs_train = eval_one_epoch(model, 
+        train_loader, 
+        loss_func,
+        stat_scores);
 
-            print(f"Epoch {e}\tTrain:\nLoss: {total_train_loss}\t\
-                Precision: {prec_train} \
-                Recall: {rec_train}\t\
-                F1: {f1_train}\t\
-                VS: {vs_train}");
-            
-            print(f"Test:\nLoss: {total_test_loss}\t\
-                Precision: {prec_test} \
-                Recall: {rec_test}\t\
-                F1: {f1_test}\t\
-                VS: {vs_test}");
-            
-            e += 1;
+        total_test_loss, prec_test, rec_test, f1_test, vs_test = eval_one_epoch(model, 
+        test_loader, 
+        loss_func,
+        stat_scores);
 
-            #if we have found a better model, save the results
-            if total_test_loss < best_loss:
-                print("New best model found...!");
-                patience = Config.EARLY_STOPPING_PATIENCE;
-                best_loss = total_test_loss;
-                best_prec = prec_test;
-                best_rec = rec_test;
-                best_f1 = f1_test;
-                best_vs = vs_test;
-            
-            #if we haven't found a better model, decrease patience
-            patience -= 1;
-            
-            #if we ran out of patience, exit the loop
-            if patience == 0:
-                break;
+        print(f"Epoch {e}\tTrain:\nLoss: {total_train_loss}\t\
+            Precision: {prec_train} \
+            Recall: {rec_train}\t\
+            F1: {f1_train}\t\
+            VS: {vs_train}");
         
-        print(f"Fold {f}:\nLoss: {best_loss}\t\
-                Precision: {best_prec} \
-                Recall: {best_rec}\t\
-                F1: {best_f1}\t\
-                VS: {best_vs}");
+        print(f"Test:\nLoss: {total_test_loss}\t\
+            Precision: {prec_test} \
+            Recall: {rec_test}\t\
+            F1: {f1_test}\t\
+            VS: {vs_test}");
+        
+        e += 1;
 
-        f += 1;
+        #if we have found a better model, save the results
+        if total_test_loss < best_loss:
+            print("New best model found...!");
+            patience = Config.EARLY_STOPPING_PATIENCE;
+            best_loss = total_test_loss;
+            best_prec = prec_test;
+            best_rec = rec_test;
+            best_f1 = f1_test;
+            best_vs = vs_test;
+        
+        #if we haven't found a better model, decrease patience
+        patience -= 1;
+        
+        #if we ran out of patience, exit the loop
+        if patience == 0:
+            break;
+    
+    print(f"Fold {f}:\nLoss: {best_loss}\t\
+            Precision: {best_prec} \
+            Recall: {best_rec}\t\
+            F1: {best_f1}\t\
+            VS: {best_vs}");
+
+    f += 1;
